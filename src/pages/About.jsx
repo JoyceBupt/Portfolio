625e67abedea9c8d37dfd0d74e6365a7c68d0495
JoyceBupt <JoyceBupt@gmail.com>
2025-01-11 04:04:42 +0800

    feat: Projects Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,17 +12,17 @@ const About = () => {
   return (
     <section className='max-container'>
       <h1 className='head-text'>
-        Hello, I'm{" "}
+        Hello, Here's{" "}
         <span className='blue-gradient_text font-semibold drop-shadow'>
           {" "}
-          Adrian
+          BUPT
         </span>{" "}
         👋
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Engineer based in Croatia, specializing in technical
+          Software Engineer based in BUPT, specializing in technical
           education through hands-on learning and building applications.
         </p>
       </div>
